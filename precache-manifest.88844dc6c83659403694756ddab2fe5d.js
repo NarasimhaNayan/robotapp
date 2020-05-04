a327548809afeaaa7250066512aac2873fa10b31
Nayan Bhushan <nayanbhushankn@gmail.com>
2020-05-04 19:48:13 +0530

    Updates

---
+++ precache-manifest.88844dc6c83659403694756ddab2fe5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494b3a0901f372108a2ad8df9c769a73",
+    "url": "/robotapp/index.html"
+  },
+  {
+    "revision": "537e8a2237e1150a84cd",
+    "url": "/robotapp/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "501309c2e172657f1f0a",
+    "url": "/robotapp/static/css/main.620d305b.chunk.css"
+  },
+  {
+    "revision": "537e8a2237e1150a84cd",
+    "url": "/robotapp/static/js/2.723846ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotapp/static/js/2.723846ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501309c2e172657f1f0a",
+    "url": "/robotapp/static/js/main.18a690ac.chunk.js"
+  },
+  {
+    "revision": "6a33010f9c63a8f08e74",
+    "url": "/robotapp/static/js/runtime-main.8addbfc7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
