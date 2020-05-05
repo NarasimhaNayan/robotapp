57602ce93396cd67bbea48b94a39a9db35a3421b
Nayan Bhushan <nayanbhushankn@gmail.com>
2020-05-05 12:11:29 +0530

    Correction 1: used json placeholder

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Searchbox from '../Components/Searchbox';
 import CardList from '../Components/CardList' ;
-import {robots} from '../Components/robots';
+// import {robots} from '../Components/robots';
 import './app.css';
 import Scroll from '../Components/Scroll';
+import ErrorBoundary from '../Components/ErrorBoundary';
 
 //app owns state so use class syntax
 class App extends React.Component {
@@ -15,43 +16,49 @@ class App extends React.Component {
 		searchfield:''
 		}
 	}
+	
+		
+	
+	//to make webpage realistic
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => {this.setState({robots:users})});
+				
+
+
+	}
+		
+	
+	
 	//to know the change
 	onSearchChange = (event) => {
 
 		this.setState({searchfield: event.target.value})
-		
 	}
-
-	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response => {
-				response.json();
-			})
-			.then(users => {
-				this.setState({robots:robots})
-			})
-		
-	}
-	
 	
 	render() {
+		//const  { robots, searchfield } = this.state;
 		const filterdrobot=this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		if(robots.length === 0)
-		{
-			return <h1>Loading</h1>
-		}else{
+		// if(robots.length === 0)
+		// {
+		// 	return <h1>Loading</h1>
+		// }
+		// else {
 			return (
 				<div className='tc'>
 					<h1 className='f1'>RoboFriends</h1>
 					<Searchbox searchChange={this.onSearchChange} />
 					<Scroll>
-						<CardList robots={filterdrobot} />
+						<ErrorBoundary>
+							<CardList robots={filterdrobot} />
+						</ErrorBoundary>
 					</Scroll>
 				</div>
 				);
-		}
+		
 		
 	}
 	
